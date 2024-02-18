b841640a1c5577c0896d771df8712a2e32e7f538
Aabhushan47 <khanalaabushan10@gmail.com>
2024-02-18 21:21:13 +0545

    project 4

---
+++ projects/number-guesser/script.js
@@ -0,0 +1,69 @@
+let randomNo = parseInt(Math.random() * 99 + 1);
+
+const userInput = document.querySelector("#guessField");
+const submit = document.querySelector("#subt");
+const result = document.querySelector(".lowOrHi");
+const prevGuess = document.querySelector(".guesses");
+const remGuess = document.querySelector(".lastResult");
+const error = document.querySelector(".error");
+
+let prevResult = [];
+let numGuess = 10;
+
+let playGame = true;
+
+if (playGame) {
+  submit.addEventListener("click", (e) => {
+    e.preventDefault();
+    const guess = parseInt(userInput.value);
+    validateGuess(guess);
+  });
+}
+
+function validateGuess(guess) {
+  if (isNaN(guess)) {
+    error.innerHTML = `Please Enter Valid Number`;
+  } else if (guess < 1) {
+    error.innerHTML = `Dont Enter Less Than 1`;
+  } else if (guess > 100) {
+    error.innerHTML = `Enter Number Less Than 100`;
+  } else {
+    prevResult.push(guess);
+    if (numGuess <= 0) {
+      displayGuess(guess);
+      displayMessage(`You Lost The Number Was ${randomNo}`);
+      endGame();
+    } else {
+      displayGuess(guess);
+      checkGuess(guess);
+    }
+  }
+}
+
+function checkGuess(guess) {
+  if (guess == randomNo) {
+    displayMessage("Yaaaaaaaaaay");
+    endGame();
+  } else if (guess > randomNo) {
+    displayMessage("Guess Lower Number");
+  } else if (guess < randomNo) {
+    displayMessage("Guess Higher Number");
+  }
+}
+
+function displayGuess(guess) {
+  userInput.value = "";
+  //   console.log(guess);
+  prevGuess.innerHTML += `${guess},`;
+  numGuess--;
+  remGuess.innerHTML = `${numGuess}`;
+}
+
+function displayMessage(message) {
+  result.innerHTML = `<h2>${message}</h2>`;
+}
+
+function endGame() {
+  userInput.value = "";
+  userInput.setAttribute("disabled", "");
+}
